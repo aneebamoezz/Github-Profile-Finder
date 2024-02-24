96fce4e8fb8dfd4e88b92c39847f814bcaec6006
aneebamoezz <anibakaleem@gmail.com>
2024-02-24 13:39:19 +0500

    commit initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GithubProfileFinder from './components/GithubProfileFinder';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubProfileFinder />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GithubProfileFinder.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import UserCard from "./UserCard";
+import "./styles.css";
+
+export default function GithubProfileFinder() {
+  const [userName, setUserName] = useState("aneebamoezz");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchGithubUserData() {
+    setLoading(true);
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+    if (data) {
+      setUserData(data);
+      setLoading(false);
+      setUserName("");
+    }
+
+    //console.log(data);
+  }
+
+  function handleSubmit() {
+    fetchGithubUserData();
+  }
+
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
+  if (loading) {
+    return <h1>Loading data ! Please wait</h1>;
+  }
+
+  return (
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          name="search-by-username"
+          type="text"
+          placeholder="Search Gthub Username..."
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      {userData !== null ? <UserCard user={userData} /> : null}
+    </div>
+  );
+}
